db43e2f0e36b3f032145b0732cba4a51d0086812
Niro <niro4ce@gmail.com>
2023-09-19 20:08:38 +0800

    Let's try it

---
+++ src/main.rs
@@ -0,0 +1,57 @@
+use anyhow::anyhow;
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::model::gateway::Ready;
+use serenity::prelude::*;
+use shuttle_secrets::SecretStore;
+use tracing::{error, info};
+
+struct Bot;
+
+#[async_trait]
+impl EventHandler for Bot {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content.contains("https://twitter.com") {
+            
+            let user = msg.author.name.as_str();
+
+            let message = msg.content.replace("https://twitter.com", "https://vxtwitter.com");
+            
+            let bot_response = format!("From: **{}**\n\n{}", user, message);
+
+            if let Err(e) = msg.channel_id.say(&ctx.http, bot_response).await {
+                error!("Error sending message: {:?}", e);
+            }
+
+            if let Err(e) = msg.delete(ctx.http).await {
+                error!("Error deleting original message: {:?}", e);
+            }
+        }
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        info!("{} is connected!", ready.user.name);
+    }
+}
+
+#[shuttle_runtime::main]
+async fn serenity(
+    #[shuttle_secrets::Secrets] secret_store: SecretStore,
+) -> shuttle_serenity::ShuttleSerenity {
+    // Get the discord token set in `Secrets.toml`
+    let token = if let Some(token) = secret_store.get("DISCORD_TOKEN") {
+        token
+    } else {
+        return Err(anyhow!("'DISCORD_TOKEN' was not found").into());
+    };
+
+    // Set gateway intents, which decides what events the bot will be notified about
+    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
+
+    let client = Client::builder(&token, intents)
+        .event_handler(Bot)
+        .await
+        .expect("Err creating client");
+
+    Ok(client.into())
+}
